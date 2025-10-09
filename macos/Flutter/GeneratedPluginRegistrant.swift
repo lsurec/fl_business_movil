7c295f9ffb1fc2feb8dafd7b5a8e64a4959b65e1
B3RERT <robertmorales181@gmail.com>
2025-10-09 09:46:04 -0600

    feat: Initial commit of blue_thermal_printer plugin with iOS, Android, and macOS support
    - Added pubspec.yaml and pubspec.lock for package configuration
    - Implemented iOS plugin with Swift and Objective-C support
    - Implemented macOS plugin with Swift and Objective-C support
    - Created Android plugin with necessary Gradle configurations
    - Added core functionality for Bluetooth thermal printing
    - Included example application for demonstration
    - Added test cases for method channel communication

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,13 @@
 import FlutterMacOS
 import Foundation
 
+import blue_thermal_printer
 import flutter_image_compress_macos
 import path_provider_foundation
 import printing
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BlueThermalPrinterPlugin.register(with: registry.registrar(forPlugin: "BlueThermalPrinterPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
