a05a415c28f022ef2f69375baf6f6d57e29518d5
B3RERT <robertmorales181@gmail.com>
2025-10-10 14:49:50 -0600

    feat: Reemplazar la lógica de impresión de tickets con el generador de ESC/POS

--- lib/service/impresion_ticket.dart
+++ lib/service/impresion_ticket.dart
@@ -4,6 +4,7 @@ import 'package:fl_business/utils/ticket_utils.dart';
 import 'package:fl_business/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
+import 'package:flutter_esc_pos_utils/flutter_esc_pos_utils.dart';
 
 class ImpresionTicket {
   /// Método estático para imprimir directamente en ticket
@@ -31,59 +32,47 @@ class ImpresionTicket {
       bool conectado = await ticketUtils.conectarImpresora(context, impresora);
       if (!conectado) return;
 
-      // Cargar e imprimir logo automáticamente
-      bluetooth.printNewLine();
+      List<int> bytes = [];
 
-      //Con este codigo se agregaría el logo:
-      // final Uint8List? logoBytesEmp = await UtilitiesService.loadLogoImage(
-      //   '', //path completo generado con el campo de la empresa seleccionada
-      // );
-      // if (logoBytesEmp != null) {
-      //   final Uint8List? imagenReducida = await utils.prepararImagenParaImpresion(
-      //     logoBytesEmp,
-      //   );
-      //   if (imagenReducida != null) {
-      //     await bluetooth.printImageBytes(imagenReducida);
-      //   }
-      // }
-
-      bluetooth.printNewLine();
-
-      // Imprimir encabezado (empresa, NIT, dirección) de la empresa seleccionada
-      await ticketUtils.imprimirTexto('ILGUA', size: 2, align: 1);
-      await ticketUtils.imprimirTexto('NIT: 15613-7', size: 1, align: 1);
-      await ticketUtils.imprimirTexto('Zona 14-7', size: 1, align: 1);
-      bluetooth.printNewLine();
-
-      // Imprimir cada movimiento
-      for (final m in movimientos) {
-        await ticketUtils.imprimirTexto('Fecha: ${m.fecha}');
-        await ticketUtils.imprimirTexto('Detalle: ${m.detalle}');
-        await ticketUtils.imprimirTexto(
-          'Débito: ${m.debito.toStringAsFixed(2)}  Crédito: ${m.credito.toStringAsFixed(2)}',
-        );
-        await ticketUtils.imprimirTexto('Saldo: ${m.saldo.toStringAsFixed(2)}');
-        await ticketUtils.imprimirTexto(
-          'Documento: ${m.documento}  Tipo: ${m.tipo}',
-        );
-        await ticketUtils.imprimirTexto('Referencia: ${m.referencia}');
-        bluetooth.printNewLine();
-      }
-      await ticketUtils.imprimirTexto(
-        'Generado por: ${UtilitiesService.nombreEmpresa}',
-        size: 0,
-        align: 1,
-      );
-      await ticketUtils.imprimirTexto(
-        '${UtilitiesService.version}',
-        size: 0,
-        align: 1,
+      final generator = Generator(
+        PaperSize.mm58,
+        await CapabilityProfile.load(),
       );
 
-      // Línea final y desconexión
-      bluetooth.printNewLine();
-      bluetooth.printNewLine();
-      bluetooth.printNewLine();
+      bytes += generator.setGlobalCodeTable('CP1252');
+
+      bytes += generator.text(
+        "Prueba",
+        styles: PosStyles(
+          align: PosAlign.center,
+          width: PosTextSize.size1,
+          height: PosTextSize.size1,
+        ),
+      );
+
+      bytes += generator.hr();
+
+      bytes += generator.text(
+        "CENTRADO",
+        styles: PosStyles(align: PosAlign.center),
+      );
+
+      bytes += generator.text(
+        "IZQUIERDA",
+        styles: PosStyles(align: PosAlign.left),
+      );
+
+      bytes += generator.text(
+        "DERECHA",
+        styles: PosStyles(align: PosAlign.right),
+      );
+
+      bytes += generator.text("normal", styles: PosStyles(bold: false));
+
+      bytes += generator.text("Bool", styles: PosStyles(bold: true));
+
+      // Envíalo a la impresora
+      await bluetooth.writeBytes(Uint8List.fromList(bytes));
 
       await ticketUtils.desconectarSiConectado();
     } catch (e) {
